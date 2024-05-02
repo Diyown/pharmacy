8134622082eab67034afc9a43aa84fae6f1a3bb4
XX528@ccs.local <XX528@ccs.local>
2024-05-02 13:30:25 -0700

    new update

--- admin.php
+++ admin.php
@@ -10,10 +10,10 @@
 			margin: 0;
 			font-family: Arial, sans-serif;
 		}
-
+/*==========NAVIGATION SIDE BAR=============*/
 		.sidebar {
 			height: 100%;
-			width: 200px;
+			width: 300px;
 			position: fixed;
 			top: 0;
 			left: 0;
@@ -37,19 +37,49 @@
 			margin-left: 200px;
 			padding: 20px;
 		}
+		.sidebar h2 {
+			color: #fff; 
+			font-family: "Arial Black", sans-serif; 
+			font-size: 24px; 
+			text-align: left; 
+			margin-bottom: 20px; 
+			margin-left: 20px;
+		}
+/*===========CONTENT=============*/
+		.content {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh; 
+		}
+		.wrapper{
+			width: 300px;
+			height: 150px;
+			padding: 15px;
+			background-color: #ede9e8;
+			box-shadow: 10px 15px 10px grey;
+			border-radius: 10px;
+			
+			border-color: black;
+			
+		}
 	</style>
 	
 </head>
 <body>
     <div class="sidebar">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+		<h2>ADMIN DASHBOARD</h2>
+        <a href="admin.php">Home</a>
+        <a href="#about">Order</a>
+        <a href="#services">Order History</a>
+        <a href="#contact">Stocks</a>
+		<a href="login.php">Log Out</a>
     </div>
-
+<!--============CONTENT==============-->
     <div class="content">
-        <!-- Your main content goes here -->
+		<form class="wrapper">
+			<p>the JUDE BOTIKA Admin Dashboard. This dashboard provides you with the tools and information you need to effectively manage the operations of our pharmacy.</p>
+		</form>
     </div>
 </body>
 </body>
